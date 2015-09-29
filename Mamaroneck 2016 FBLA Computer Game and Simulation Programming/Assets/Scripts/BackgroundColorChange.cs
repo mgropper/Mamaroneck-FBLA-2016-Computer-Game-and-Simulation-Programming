6eba8634e982eabe9c20a17f9c8340df1f446e09
mgropper <Matt@Matts-MBP.home>
2015-09-28 21:41:34 -0400

    Created Menu V1.0

---
+++ Mamaroneck 2016 FBLA Computer Game and Simulation Programming/Assets/Scripts/BackgroundColorChange.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+/**
+ * Sets the Background color to a random color
+ **/
+public class BackgroundColorChange : MonoBehaviour {
+	public Camera camera;
+	public Color zero = Color.red;
+	public Color one = Color.red;
+	public Color two = Color.red;
+	public Color three = Color.red;
+	public Color four = Color.red;
+	public Color five = Color.red;
+	// Use this for initialization
+	void Start () {
+		int rand = (int)(Random.value * 10) / 2;
+		Debug.Log (rand); //shows value to console
+		if (rand == 0) {
+			camera.backgroundColor = zero;
+		} else if (rand == 1) {
+			camera.backgroundColor = one;
+		} else if (rand == 2) {
+			camera.backgroundColor = two;			
+		} else if (rand == 3) {
+			camera.backgroundColor = three;			
+		} else if (rand == 4) {
+			camera.backgroundColor = four;			
+		} else if (rand == 5) {
+			camera.backgroundColor = five;			
+		}
+	}
+}
---
+++ Mamaroneck 2016 FBLA Computer Game and Simulation Programming/Assets/Scripts/MenuTextAndButtons.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuTextAndButtons : MonoBehaviour {
+	public string gameName;
+	public Texture buttonPlayTexture;
+	public Texture buttonQuitTexture;
+	public GUIStyle style;
+	 
+	void OnGUI () {
+		if (GUI.Button (new Rect (Screen.width / 2 - 100, Screen.height - 300, 200, 125), buttonPlayTexture)) { //create button to play game
+			Application.LoadLevel(1);
+		}
+		if (GUI.Button (new Rect (Screen.width / 2 - 100, Screen.height - 150, 200, 125), buttonQuitTexture)) { //create button to play game
+			Application.Quit();
+		}
+		GUI.Label (new Rect (Screen.width / 2, 30, 250, 250), gameName, style); //create text at top with game name
+	}
+}
