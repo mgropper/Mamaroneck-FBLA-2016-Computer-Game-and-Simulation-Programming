524bd8c634b8762469df75eedce5d799ec9e14e4
mgropper <Matt@Matts-MBP.home>
2015-10-08 23:49:55 -0400

    Menu V1.1.1
    added a clear background to the play/quit buttons

--- Mamaroneck 2016 FBLA Computer Game and Simulation Programming/Assets/Scripts/MenuTextAndButtons.cs
+++ Mamaroneck 2016 FBLA Computer Game and Simulation Programming/Assets/Scripts/MenuTextAndButtons.cs
@@ -8,17 +8,18 @@ public class MenuTextAndButtons : MonoBehaviour {
 	public string gameName;
 	public Texture buttonPlayTexture;
 	public Texture buttonQuitTexture;
-	public GUIStyle style;
-	 
+	public GUIStyle textStyle;
+	public GUIStyle rectStyle;
+
 	void OnGUI () {
 		if (guiMenuEnabled) {
-			if (GUI.Button (new Rect (Screen.width / 2 - 225, Screen.height - 200, 200, 125), buttonPlayTexture)) { //create button to play game
+			if (GUI.Button (new Rect (Screen.width / 2 - 225, Screen.height - 200, 200, 125), buttonPlayTexture, rectStyle)) { //create button to play game
 				guiMenuEnabled = false;
 			}
-			if (GUI.Button (new Rect (Screen.width / 2 + 25, Screen.height - 200, 200, 125), buttonQuitTexture)) { //create button to quit game
+			if (GUI.Button (new Rect (Screen.width / 2 + 25, Screen.height - 200, 200, 125), buttonQuitTexture, rectStyle)) { //create button to quit game
 				Application.Quit ();
 			}
-			GUI.Label (new Rect (Screen.width / 2, 30, 250, 250), gameName, style); //create text at top with game name
+			GUI.Label (new Rect (Screen.width / 2 - 100, 30, 250, 250), gameName, textStyle); //create text at top with game name
 		}
 	}
 }
