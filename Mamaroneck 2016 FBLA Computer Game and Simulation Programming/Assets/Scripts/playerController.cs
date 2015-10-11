7052723055ac2d0516a687b3471b775317d0f1e2
mgropper <Matt@Matts-MBP.home>
2015-10-11 17:26:05 -0400

    Created Animations
    added animations to make the player walk, jump and idle look smoother

--- Mamaroneck 2016 FBLA Computer Game and Simulation Programming/Assets/Scripts/playerController.cs
+++ Mamaroneck 2016 FBLA Computer Game and Simulation Programming/Assets/Scripts/playerController.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class playerController : MonoBehaviour {
-
+	bool move = true;
+	public GameObject player;
 	public float moveSpeed;
 	public float jumpHeight;
 	private bool facingRight;
@@ -12,40 +13,55 @@ public class playerController : MonoBehaviour {
 	public float groundCheckRadius;
 	public LayerMask whatIsGround;
 	private bool grounded;
+	private bool once = true;
+	private Animator anim;
 
 	// Use this for initialization
 	void Start () {
-		h = CrossPlatformInputManager.GetAxis("Horizontal");
 		facingRight = false;
+		anim = GetComponent<Animator> ();
 	}
 
 	void FixedUpdate(){
+
 		//Vector2 pos = new Vector2(groundCheck.transform.position.x, groundCheck.position.y);
 		//grounded = GetComponent<Physics2D>().OverlapCircle(pos, groundCheckRadius, whatIsGround);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x,jumpHeight);
-		}
-		if (Input.GetKey (KeyCode.D)) {
-			if(facingRight && h < 0){
-				Flip ();
+		h = CrossPlatformInputManager.GetAxis("Horizontal");
+		if (move) {
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jumpHeight);
 			}
-			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed,GetComponent<Rigidbody2D>().velocity.y);
-		}
-		if (Input.GetKey (KeyCode.A)) {
-			if(!facingRight && h > 0){
-				Flip ();
+			if (Input.GetKey (KeyCode.D)) {
+				if (!facingRight && once) {
+					once = false;
+					Flip ();
+					facingRight = true;
+				}
+				GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+			} 
+			if(Input.GetKeyUp(KeyCode.D)){
+				once = true;
 			}
-			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed,GetComponent<Rigidbody2D>().velocity.y);
+			if (Input.GetKey (KeyCode.A)) {
+				if (facingRight && once) {
+					once = false;
+					Flip ();
+					facingRight = false;
+				}
+				GetComponent<Rigidbody2D> ().velocity = new Vector2 (-moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+			}
+			if(Input.GetKeyUp(KeyCode.A)){
+				once = true;
+			}
+			anim.SetFloat("speed", Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
 		}
 	}
 
 	void Flip(){
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
+		player.transform.localScale = new Vector3(-player.transform.localScale.x, player.transform.localScale.y, player.transform.localScale.z);
 	}
 }
