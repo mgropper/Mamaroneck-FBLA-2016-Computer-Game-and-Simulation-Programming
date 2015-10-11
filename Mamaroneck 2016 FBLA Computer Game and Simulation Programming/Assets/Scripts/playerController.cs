bb3a7a39e6d4a53866b607fbede789be2d060f0b
mgropper <Matt@Matts-MBP.home>
2015-10-11 13:57:27 -0400

    Created Player Input V1.0
    added functionality to the keyboard to allow the player to move left,
    right and up using a, d and space

---
+++ Mamaroneck 2016 FBLA Computer Game and Simulation Programming/Assets/Scripts/playerController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class playerController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpHeight;
+	private bool facingRight;
+	private float h;
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+	private bool grounded;
+
+	// Use this for initialization
+	void Start () {
+		h = CrossPlatformInputManager.GetAxis("Horizontal");
+		facingRight = false;
+	}
+
+	void FixedUpdate(){
+		//Vector2 pos = new Vector2(groundCheck.transform.position.x, groundCheck.position.y);
+		//grounded = GetComponent<Physics2D>().OverlapCircle(pos, groundCheckRadius, whatIsGround);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x,jumpHeight);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			if(facingRight && h < 0){
+				Flip ();
+			}
+			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed,GetComponent<Rigidbody2D>().velocity.y);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			if(!facingRight && h > 0){
+				Flip ();
+			}
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed,GetComponent<Rigidbody2D>().velocity.y);
+		}
+	}
+
+	void Flip(){
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
