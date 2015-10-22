e0ef839b2cae9222b10c4a440d2c6e8c6f429409
mgropper <mgropps17@gmail.com>
2015-10-22 12:43:35 -0400

    Added more Menu components and Started to fix Animations

--- Mamaroneck 2016 FBLA Computer Game and Simulation Programming/Assets/Scripts/MenuTextAndButtons.cs
+++ Mamaroneck 2016 FBLA Computer Game and Simulation Programming/Assets/Scripts/MenuTextAndButtons.cs
@@ -4,24 +4,43 @@ using System.Collections;
  * 
  * */
 public class MenuTextAndButtons : MonoBehaviour {
-	bool guiMenuEnabled = true;
+    public GameObject playerFake;
+    bool guiMenuEnabled = true;
 	public string gameName;
 	public Texture buttonPlayTexture;
 	public Texture buttonQuitTexture;
 	public GUIStyle textStyle;
 	public GUIStyle rectStyle;
+    public GUIStyle buttonStyle;
+    public Animator anim;
+
+    void start(){
+        anim.SetBool("isMenu", true);
+    }
 
 	void OnGUI () {
 		if (guiMenuEnabled) {
 			if (GUI.Button (new Rect (Screen.width / 2 - 225, Screen.height - 200, 200, 125), buttonPlayTexture, rectStyle)) { //create button to play game
 				guiMenuEnabled = false;
+                playerFake.SetActive(false);
 			}
 			if (GUI.Button (new Rect (Screen.width / 2 + 25, Screen.height - 200, 200, 125), buttonQuitTexture, rectStyle)) { //create button to quit game
 				Application.Quit ();
 			}
 			GUI.Label (new Rect (Screen.width / 2 - 100, 30, 250, 250), gameName, textStyle); //create text at top with game name
 		} else {
-
-		}
+            if (GUI.Button(new Rect(Screen.width / 2 - 500, Screen.height - 500, 200, 125), "1", buttonStyle))
+            { //create button to load level one
+                Application.LoadLevel(1);
+            }
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height - 500, 200, 125), "2", buttonStyle))
+            { //create button to load level 2
+                Application.LoadLevel(2);
+            }
+            if (GUI.Button(new Rect(Screen.width / 2 + 300, Screen.height - 500, 200, 125), "3", buttonStyle))
+            { //create button to load level 3
+                Application.LoadLevel(3);
+            }
+        }
 	}
 }
