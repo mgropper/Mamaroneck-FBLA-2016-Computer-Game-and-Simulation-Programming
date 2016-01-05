629c9904764f8a774ef4375dc1ee49bb20e440d8
mgropper <mgropps17@gmail.com>
2016-01-05 10:45:55 -0500

    Started to redo respawn and fixed sticking block glitch

--- Mamaroneck 2016 FBLA Computer Game and Simulation Programming/Assets/Scripts/Respawn.cs
+++ Mamaroneck 2016 FBLA Computer Game and Simulation Programming/Assets/Scripts/Respawn.cs
@@ -4,25 +4,21 @@ using System.Collections;
 public class Respawn : MonoBehaviour {
     public Transform origin;
     public GameObject player;
-    public bool checkpoint = false;
+    string area = "middle";
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    if(player.transform.position.y < 0 && player.transform.position.y <= -7) {
-            player.transform.position = origin.position;
-        } if (player.transform.position.y > 0 && player.transform.position.y < 0) {
-            player.transform.position = origin.position;
-        }
+        
 	}
 
-    void OnTriggerEnter() {
-        if (checkpoint) {
-            origin.transform.position = new Vector3(transform.localPosition.x, transform.localPosition.y + 2);
-        }
+    void OnTriggerEnter2D()
+    {
+
     }
 }
