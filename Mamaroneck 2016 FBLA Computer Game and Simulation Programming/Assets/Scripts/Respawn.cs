6387c84b31ce6f0bec43b5cccc816fc8358e35ae
mgropper <mgropps17@gmail.com>
2016-01-08 10:52:09 -0500

    Finished respawn and created level 1 bottom right

--- Mamaroneck 2016 FBLA Computer Game and Simulation Programming/Assets/Scripts/Respawn.cs
+++ Mamaroneck 2016 FBLA Computer Game and Simulation Programming/Assets/Scripts/Respawn.cs
@@ -10,6 +10,12 @@ public class Respawn : MonoBehaviour {
     public bool bottomLeft = false;
     public bool bottomRight = false;
     public bool middle = true;
+    public GameObject topLeftG;
+    public GameObject topRightG;
+    public GameObject bottomLeftG;
+    public GameObject bottomRightG;
+    public GameObject middleG;
+    public GameObject arrows;
 
     // Use this for initialization
     void Start () {
@@ -18,10 +24,10 @@ public class Respawn : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(player.transform.position.y < -2 && (area.Equals("topLeft") || area.Equals("topRight")))
+        if((area.Equals("topLeft") || area.Equals("topRight")) && player.transform.position.y < -6)
         {
             player.transform.position = origin.transform.position;
-        }else if (player.transform.position.y < -10 && (area.Equals("bottomLeft") || area.Equals("bottomRight") || area.Equals("middle")))
+        }else if ((area.Equals("bottomLeft") || area.Equals("bottomRight") || area.Equals("middle")) && player.transform.position.y < -10)
         {
             player.transform.position = origin.position;
         }
@@ -36,26 +42,58 @@ public class Respawn : MonoBehaviour {
         if (topLeft)
         {
             area = "topLeft";
-            origin.position = new Vector3(-3.7f, 3.1f);
-        }else if (topRight)
+            origin.position = new Vector3(-2.7f, 3.1f);
+            arrows.SetActive(false);
+            topLeftG.SetActive(true);
+            topRightG.SetActive(false); 
+            bottomLeftG.SetActive(false);
+            bottomRightG.SetActive(false);
+            middleG.SetActive(false);
+        }
+        else if (topRight)
         {
             area = "topRight";
             origin.position = new Vector3(5.3f, 3.1f);
+            arrows.SetActive(false);
+            topLeftG.SetActive(false);
+            topRightG.SetActive(true);
+            bottomLeftG.SetActive(false);
+            bottomRightG.SetActive(false);
+            middleG.SetActive(false);
         }
         else if (middle)
         {
             area = "middle";
             origin.position = new Vector3(1.3f, 1.1f);
+            topLeftG.SetActive(true);
+            topRightG.SetActive(true);
+            bottomLeftG.SetActive(true);
+            bottomRightG.SetActive(true);
+            middleG.SetActive(true);
         }
         else if (bottomLeft)
         {
             area = "bottomLeft";
-            origin.position = new Vector3(-3.7f, -0.9f);
+            origin.position = new Vector3(-2.7f, -0.9f);
+            arrows.SetActive(false);
+            topLeftG.SetActive(false);
+            topRightG.SetActive(false);
+            bottomLeftG.SetActive(true);
+            bottomRightG.SetActive(false);
+            middleG.SetActive(false);
         }
         else if (bottomRight)
         {
             area = "bottomRight";
             origin.position = new Vector3(5.3f, -0.9f);
+            arrows.SetActive(false);
+            topLeftG.SetActive(false);
+            topRightG.SetActive(false);
+            bottomLeftG.SetActive(false);
+            bottomRightG.SetActive(true);
+            middleG.SetActive(false);
+
         }
     }
 }
+
