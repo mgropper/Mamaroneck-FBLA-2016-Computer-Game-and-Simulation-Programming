da8d118bcef49e427d0821922cecca66a2e070bf
mgropper <mgropps17@gmail.com>
2016-01-13 13:31:27 -0500

    Finished Game Mechanics and made respawn more efficient

--- Mamaroneck 2016 FBLA Computer Game and Simulation Programming/Assets/Scripts/Respawn.cs
+++ Mamaroneck 2016 FBLA Computer Game and Simulation Programming/Assets/Scripts/Respawn.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class Respawn : MonoBehaviour {
     public Transform origin;
     public GameObject player;
-    string area = "middle";
     public bool topLeft = false;
     public bool topRight = false;
     public bool bottomLeft = false;
@@ -15,19 +14,18 @@ public class Respawn : MonoBehaviour {
     public GameObject bottomLeftG;
     public GameObject bottomRightG;
     public GameObject middleG;
-    public GameObject arrows;
 
     // Use this for initialization
     void Start () {
-	
-	}
+       
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if((area.Equals("topLeft") || area.Equals("topRight")) && player.transform.position.y < -6)
+        if((topLeft || topRight) && player.transform.position.y < -6)
         {
             player.transform.position = origin.transform.position;
-        }else if ((area.Equals("bottomLeft") || area.Equals("bottomRight") || area.Equals("middle")) && player.transform.position.y < -10)
+        }else if ((bottomLeft || bottomRight || middle) && player.transform.position.y < -10)
         {
             player.transform.position = origin.position;
         }
@@ -41,57 +39,23 @@ public class Respawn : MonoBehaviour {
     {
         if (topLeft)
         {
-            area = "topLeft";
             origin.position = new Vector3(-2.7f, 3.1f);
-            arrows.SetActive(false);
-            topLeftG.SetActive(true);
-            topRightG.SetActive(false); 
-            bottomLeftG.SetActive(false);
-            bottomRightG.SetActive(false);
-            middleG.SetActive(false);
         }
         else if (topRight)
         {
-            area = "topRight";
             origin.position = new Vector3(5.3f, 3.1f);
-            arrows.SetActive(false);
-            topLeftG.SetActive(false);
-            topRightG.SetActive(true);
-            bottomLeftG.SetActive(false);
-            bottomRightG.SetActive(false);
-            middleG.SetActive(false);
         }
         else if (middle)
         {
-            area = "middle";
             origin.position = new Vector3(1.3f, 1.1f);
-            topLeftG.SetActive(true);
-            topRightG.SetActive(true);
-            bottomLeftG.SetActive(true);
-            bottomRightG.SetActive(true);
-            middleG.SetActive(true);
         }
         else if (bottomLeft)
         {
-            area = "bottomLeft";
             origin.position = new Vector3(-2.7f, -0.9f);
-            arrows.SetActive(false);
-            topLeftG.SetActive(false);
-            topRightG.SetActive(false);
-            bottomLeftG.SetActive(true);
-            bottomRightG.SetActive(false);
-            middleG.SetActive(false);
         }
         else if (bottomRight)
         {
-            area = "bottomRight";
             origin.position = new Vector3(5.3f, -0.9f);
-            arrows.SetActive(false);
-            topLeftG.SetActive(false);
-            topRightG.SetActive(false);
-            bottomLeftG.SetActive(false);
-            bottomRightG.SetActive(true);
-            middleG.SetActive(false);
 
         }
     }
---
+++ Mamaroneck 2016 FBLA Computer Game and Simulation Programming/Assets/Scripts/gameMechanics.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameMechanics : MonoBehaviour {
+    public GameObject player;
+    public bool topLeft = false;
+    public bool topRight = false;
+    public bool bottomLeft = false;
+    public bool bottomRight = false;
+    public bool middle = true;
+    public bool piece = false;
+    public bool hasObject = true;
+    public GameObject topLeftG;
+    public GameObject topRightG;
+    public GameObject bottomLeftG;
+    public GameObject bottomRightG;
+    public GameObject middleG;
+    public GameObject pieceG;
+    public Sprite pants;
+    public Sprite tie;
+    public Sprite jacket;
+    public Sprite shoes;
+
+    // Use this for initialization
+    void Start () {
+        middleG.SetActive(true);
+        topRightG.SetActive(true);
+        topLeftG.SetActive(false);
+        bottomRightG.SetActive(false);
+        bottomLeftG.SetActive(false);
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D()
+    {
+        if (middle)
+        {
+            if (hasObject && topRight)
+            {
+                topLeftG.SetActive(false);
+                topRightG.SetActive(false);
+                bottomLeftG.SetActive(false);
+                bottomRightG.SetActive(true);
+            }
+            else if (hasObject && topLeft)
+            {
+                topLeftG.SetActive(false);
+                topRightG.SetActive(false);
+                bottomLeftG.SetActive(true);
+                bottomRightG.SetActive(false);
+            }
+            else if (hasObject && bottomLeft)
+            {
+                Application.LoadLevel(2);
+            }
+            else if (hasObject && bottomRight)
+            {
+                {
+                    topLeftG.SetActive(true);
+                    topRightG.SetActive(false);
+                    bottomLeftG.SetActive(false);
+                    bottomRightG.SetActive(false);
+                }
+            }
+        }
+        else if (topRight)
+        {
+            middleG.SetActive(false);
+            if (hasObject)
+            {
+                middleG.SetActive(true);
+                player.GetComponent<SpriteRenderer>().sprite = pants;
+            }
+        }
+        else if (topLeft)
+        {
+            middleG.SetActive(false);
+            if (hasObject)
+            {
+                middleG.SetActive(true);
+                player.GetComponent<SpriteRenderer>().sprite = jacket;
+            }
+        }
+        else if (bottomLeft)
+        {
+
+            middleG.SetActive(false);
+            if (hasObject)
+            {
+                middleG.SetActive(true);
+                player.GetComponent<SpriteRenderer>().sprite = tie;
+            }
+        }
+        else if (bottomRight)
+        {
+            middleG.SetActive(false);
+            if (hasObject)
+            {
+                middleG.SetActive(true);
+                player.GetComponent<SpriteRenderer>().sprite = shoes;
+            }
+
+        }
+        else if (piece)
+        {
+            hasObject = true;
+
+        }
+    }
+}
